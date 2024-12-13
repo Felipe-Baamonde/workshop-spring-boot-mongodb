1e33555950b6793ec53c497532c90c2519bc3156
Felipe-Baamonde <felipe.baamonde@sptech.school>
2024-12-13 14:49:33 -0300

    Referenciando os posts do usuário

--- src/main/java/com/felipebaamonde/workshopmongo/config/Instantiation.java
+++ src/main/java/com/felipebaamonde/workshopmongo/config/Instantiation.java
@@ -36,5 +36,8 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, sdf.parse("23/12/2024"),"Partiu viagem","Vou viajar para São Paulo. Abraços!", new AuthorDto(maria));
         Post post2 = new Post(null, sdf.parse("26/12/2024"),"Melhor almoço","Acabei de fazer uma feijoada incrível!", new AuthorDto(maria));
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1,post2));
+        userRepository.save(maria);
     }
 }
