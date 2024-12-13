218f30343c31bacdcf172b69dbb7a3166cbab565
Felipe-Baamonde <felipe.baamonde@sptech.school>
2024-12-13 12:20:01 -0300

    Retornando usuário por id

--- src/main/java/com/felipebaamonde/workshopmongo/controller/UserController.java
+++ src/main/java/com/felipebaamonde/workshopmongo/controller/UserController.java
@@ -6,6 +6,7 @@ import com.felipebaamonde.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<UserDto>> findAll(){
         return ResponseEntity.ok().body(userService.findAll().stream().map(x -> new UserDto(x)).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> findById(@PathVariable String id){
+        return ResponseEntity.ok().body(new UserDto(userService.findById(id)));
     }
 }
--- src/main/java/com/felipebaamonde/workshopmongo/services/UserService.java
+++ src/main/java/com/felipebaamonde/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.felipebaamonde.workshopmongo.services;
 
 import com.felipebaamonde.workshopmongo.entities.User;
+import com.felipebaamonde.workshopmongo.exceptions.ObjectNotFoundException;
 import com.felipebaamonde.workshopmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,5 +14,9 @@ public class UserService {
     private UserRepository userRepository;
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public User findById(String id){
+        return userRepository.findById(id).orElseThrow( () -> new ObjectNotFoundException("Usuário com id %s não encontrado".formatted(id)));
     }
 }
