0dfb42a821525d1ba9ed9978f242eae57be0d4a6
Felipe-Baamonde <felipe.baamonde@sptech.school>
2024-12-13 11:40:55 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/felipebaamonde/workshopmongo/config/Instantiation.java
@@ -0,0 +1,24 @@
+package com.felipebaamonde.workshopmongo.config;
+
+import com.felipebaamonde.workshopmongo.entities.User;
+import com.felipebaamonde.workshopmongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
